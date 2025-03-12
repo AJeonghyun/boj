b856f1f39a6fc43344a39a68381a7151b815d921
Junghyun An <daniel7806@naver.com>
2025-03-12 17:55:21 +0900

    add map

--- 큰돌/adj.cpp
+++ 큰돌/adj.cpp
@@ -10,14 +10,17 @@ vector<int> adj[V];
 int visited[V];
 
 void go(int idx) {
-  cout << idx << "\n";
+  cout << idx << '\n';
   visited[idx] = 1;
-  for(int there : adj[idx]) {
+  for(int there:adj[idx]){
     if(visited[there]) continue;
     go(there);
   }
   return;
 }
+
+
+
 int main() {
   adj[1].push_back(2);
   adj[2].push_back(1);
@@ -28,8 +31,7 @@ int main() {
   adj[3].push_back(4);
   adj[4].push_back(3);
 
-  for(int i = 0; i < V; i++) {
-    if(adj[i].size() && visited[i] == 0) go(i);
+  for(int i=0; i<V; i++) {
+    if(adj[i].size() && visited[i]==0) go(i);
   }
 }
-
